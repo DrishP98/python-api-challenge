32ec3a168819091f2fd7d73f747b737ff93172be
DrishP98 <147461997+DrishP98@users.noreply.github.com>
2024-01-07 13:51:11 +0800

    final

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "08ef805ffebe0e19152922643a6d5c90"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "99a7375c50da43baaaba52f96bee4b40"
